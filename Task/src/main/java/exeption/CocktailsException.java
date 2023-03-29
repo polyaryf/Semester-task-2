16480eb35bcac64c9564ab890e2bbacc11ec92e5
Polina Ryftina <pryftina@gmail.com>
2023-03-29 11:05:47 +0300

    delete Task

--- Task/src/main/java/exeption/CocktailsException.java
+++
@@ -1,7 +0,0 @@
-package exeption;
-
-public class CocktailsException extends Exception{
-    public CocktailsException(String message) {
-        super(message);
-    }
-}
--- Task/src/main/java/services/СocktailsService.java
+++
@@ -1,9 +0,0 @@
-package services;
-
-import model.Beverage;
-
-import java.io.IOException;
-
-public interface СocktailsService {
-    void makeСocktail(Beverage beverage) throws IOException;
-}
